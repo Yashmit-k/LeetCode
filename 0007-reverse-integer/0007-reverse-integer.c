b434f067d4a6885eeefbf66bf7e2b72efc5327f2
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2024-10-27 17:57:43 +0530

    Time: 7 ms (5.44%), Space: 8.5 MB (45.62%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,18 @@
+int reverse(int x) {
+    long long int rev_num = 0;
+    
+    while (x != 0) {
+        int digit = x % 10;
+        x /= 10;
+        if (rev_num > INT_MAX / 10 || (rev_num == INT_MAX / 10 && digit > 7)) {
+            return 0; 
+        }
+        if (rev_num < INT_MIN / 10 || (rev_num == INT_MIN / 10 && digit < -8)) {
+            return 0;
+        }
+
+        rev_num = rev_num * 10 + digit;
+    }
+    
+    return (int)rev_num;
+}
