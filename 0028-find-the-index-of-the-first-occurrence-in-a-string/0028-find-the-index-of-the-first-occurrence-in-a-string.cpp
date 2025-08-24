99a39b390a941fa399a8bad4a031cd196c4a155f
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2025-08-24 12:46:39 +0530

    Time: 2 ms (9.32%), Space: 8.8 MB (31.04%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,9 +4,7 @@ public:
         int length=haystack.length(),length2=needle.length(),idx=-1;
         for(int i=0;i<length;i++){
             string t=haystack.substr(i,length2);
-            cout<<t<<"  "<<i<<"\n";
             if(t==needle){
-                cout<<"hi\n";
                 idx=i;
                 break;
             }
