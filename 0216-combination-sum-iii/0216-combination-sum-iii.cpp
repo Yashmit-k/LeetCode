6cc9ebf361db56b1743376ca1fcb108a9e18dd62
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-06-07 16:19:57 +0530

    Time: 4 ms (7.59%), Space: 14.6 MB (5.57%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> y;
+    void a(vector<int> x,int sum,int l,int k,int n){
+        if(x.size()==k){
+            if(sum==n){
+                y.push_back(x);
+                x.clear();
+                sum=0;
+                return;
+            }
+            else{
+                return;
+            }
+        }
+        if(sum>=n){
+            return;
+        }
+        
+        if(l>9){
+            return;
+        }
+        else{
+            x.push_back(l);
+            a(x,sum+l,l+1,k,n);
+            x.pop_back();
+            a(x,sum,l+1,k,n);
+        }
+        }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        a({},0,1,k,n);
+        return y;
+    }
+};
