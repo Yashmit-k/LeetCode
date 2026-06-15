8cd203a7b903f29a90107e086cfdbe2c17527583
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-06-15 19:07:22 +0530

    Time: 1 ms (2.39%), Space: 9.4 MB (14.32%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int t=0,b=matrix.size()-1,l=0,r=matrix[0].size()-1;
+        vector<int> a;
+
+        int l=0,r=matrix[0].size()-1,t=0,b=matrix.size()-1;
         while(l<=r && t<=b){
             for(int i=l;i<=r;i++){
-                ans.push_back(matrix[t][i]);
+                cout<<matrix[t][i]<<endl;
+                a.push_back(matrix[t][i]);
             }
-            t++;
-            for(int i=t;i<=b;i++){
-                ans.push_back(matrix[i][r]);
+            cout<<"---"<<endl;
+            for(int i=t+1;i<=b;i++){
+                cout<<matrix[i][r]<<endl;
+                a.push_back(matrix[i][r]);
             }
-            r--;
-            if(t<=b){
-            for(int i=r;i>=l;i--){
-                ans.push_back(matrix[b][i]);
+            cout<<"---"<<endl;
+            if(t<b){
+                for(int i=r-1;i>=l;i--){
+                    cout<<matrix[b][i]<<endl;
+                    a.push_back(matrix[b][i]);
+                }
             }
-            b--;
+            
+            cout<<"---"<<endl;
+            if(l<r){
+                for(int i=b-1;i>t;i--){
+                    cout<<matrix[i][l]<<endl;
+                    a.push_back(matrix[i][l]);
+                }
             }
-            if(l<=r){
-            for(int i=b;i>=t;i--){
-                ans.push_back(matrix[i][l]);
-            }
+            
+            cout<<"----------"<<endl;
             l++;
-            }
+            r--;
+            b--;
+            t++;
         }
-        return ans;
+
+        return a;
     }
 };
