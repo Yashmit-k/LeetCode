02a9491b5d06c7da3a063914752c5ba293a72e9c
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-06-15 19:07:51 +0530

    Time: 3 ms (0.81%), Space: 9.3 MB (80.2%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -9,28 +9,26 @@ public:
                 cout<<matrix[t][i]<<endl;
                 a.push_back(matrix[t][i]);
             }
-            cout<<"---"<<endl;
+   
             for(int i=t+1;i<=b;i++){
                 cout<<matrix[i][r]<<endl;
                 a.push_back(matrix[i][r]);
             }
-            cout<<"---"<<endl;
+
             if(t<b){
                 for(int i=r-1;i>=l;i--){
                     cout<<matrix[b][i]<<endl;
                     a.push_back(matrix[b][i]);
                 }
             }
-            
-            cout<<"---"<<endl;
+   
             if(l<r){
                 for(int i=b-1;i>t;i--){
                     cout<<matrix[i][l]<<endl;
                     a.push_back(matrix[i][l]);
                 }
             }
-            
-            cout<<"----------"<<endl;
+
             l++;
             r--;
             b--;
