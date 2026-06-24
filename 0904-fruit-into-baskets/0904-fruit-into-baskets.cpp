e92ad905d1ea3c0aa2d1c7e6554b1d890da64d27
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-06-24 16:31:31 +0530

    Time: 54 ms (17.62%), Space: 85 MB (33.64%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int> m;
+        int count=0,mcount=0,l=0;
+        cout<<m.size();
+        for(auto i:fruits){
+            if(m.find(i)==m.end()){
+                if(m.size()<2){
+                    m[i]++;
+                    count++;
+                }
+                else{
+                    while(m.size()==2){
+                        m[fruits[l]]--;
+                        if(m[fruits[l]]==0) m.erase(fruits[l]);
+                        l++;
+                        count--;
+                    }
+                    m[i]++;
+                    count++;
+                }
+            }
+            else{
+                m[i]++;
+                count++;
+            }
+            mcount=max(count,mcount);
+        }
+        return mcount;
+    }
+};
