66c35f4b1245bcb36b96714595081b016ffbc05d
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-07-07 08:12:22 +0530

    Time: 0 ms (100%), Space: 12.6 MB (39.78%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,25 +5,30 @@ public:
         if(n==1){
             return 0;
         }
-        int c=0;
-        for(int i=0;i<n;i++){   
-            if(i==0){
-                if(nums[i]>nums[i+1]){
-                    return i;
+        int l=0,r=n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(mid==n-1){
+                if(nums[mid]>nums[mid-1]){
+                    return mid;
                 }
-                continue;
-                
+                else{
+                    r=mid-1;
+                }
             }
-            if(i==n-1 ){
-                if(nums[i]>nums[i-1])return i;
-                continue;
-                
+            else if(mid==0){
+                if(nums[mid]>nums[mid+1])return mid;
+                else l=mid+1;
             }
-            else if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                return i;
-                
+            else if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])return mid;
+            else{
+                if(nums[mid]>nums[mid-1]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
             }
-            
         }
         return 0;
     }
