af799f3768342554e23216b1ec52523149c6a87d
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-07-16 20:08:29 +0000

    Time: 66 ms (8.43%), Space: 61.8 MB (5.04%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool check(vector<int> a,vector<int> b){
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i]) return false;
+        }
+        return true;
+    }
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> a(26,0);
+        vector<int> b(26,0);
+        vector<int> ans;
+        for(auto i:p){
+            a[i-'a']++;
+        }
+        int size=p.length(),l=0;
+        for(int r=0;r<s.length();r++){
+            b[s[r]-'a']++;
+            if(r-l+1==size){
+                if(check(a,b)){
+                    ans.push_back(l);
+                }
+                b[s[l]-'a']--;
+                l++;
+            }
+        }
+        return ans;
+    }
+};
