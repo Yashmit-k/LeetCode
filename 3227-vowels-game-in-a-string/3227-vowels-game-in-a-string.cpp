27e4211e4025d2b104af969fbeb1c823bb13bee5
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-07-18 14:20:52 +0000

    Time: 10 ms (28.64%), Space: 19 MB (55.47%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -3,9 +3,9 @@ public:
     bool doesAliceWin(string s) {
         int count=0;
         for(auto i:s){
-            if(i=='a' || i=='e' || i=='i' || i=='o' || i=='u')count++;
+            if(i=='a' || i=='e' || i=='i' || i=='o' || i=='u')return true;
         }
-        if(count==0)return false;
-        return true;
+        return false;
+
     }
 };
