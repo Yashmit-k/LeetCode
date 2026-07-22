b40bfd822a0a69f42e762f7f624c53e24372cf39
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-07-22 18:44:15 +0000

    Time: 44 ms (68.74%), Space: 20.8 MB (82.83%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -13,22 +13,21 @@ public:
             else
                 return dp[i][j1][j2] = grid[i][j1] + grid[i][j2];
         }
-        int maxi=INT_MIN;
-        for(int d1=-1;d1<=1;d1++){
-            
-            for(int d2=-1;d2<=1;d2++){
+        int maxi = INT_MIN;
+        for (int d1 = -1; d1 <= 1; d1++) {
+
+            for (int d2 = -1; d2 <= 1; d2++) {
                 int value;
-                if(j1==j2){
-                    value=grid[i][j2];
+                if (j1 == j2) {
+                    value = grid[i][j2];
+                } else {
+                    value = grid[i][j2] + grid[i][j1];
                 }
-                else{
-                    value=grid[i][j2]+grid[i][j1];
-                }
-                value+=a(grid,dp,i+1,j1+d1,j2+d2);
-                maxi=max(maxi,value);
+                value += a(grid, dp, i + 1, j1 + d1, j2 + d2);
+                maxi = max(maxi, value);
             }
         }
-        return dp[i][j1][j2]=maxi;
+        return dp[i][j1][j2] = maxi;
     }
     int cherryPickup(vector<vector<int>>& grid) {
         n = grid.size();
@@ -36,6 +35,6 @@ public:
         vector<vector<vector<int>>> dp(
             n, vector<vector<int>>(m, vector<int>(m, -1)));
 
-        return a(grid,dp,0,0,m-1);
+        return a(grid, dp, 0, 0, m - 1);
     }
 };
