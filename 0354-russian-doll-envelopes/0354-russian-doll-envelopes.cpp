a502c9bacedcecc9b65fb0a134d89d4daba65019
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-07-25 20:23:11 +0000

    Time: 32 ms (97.14%), Space: 81.7 MB (76.89%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(),envelopes.end(),[](vector<int>& a,vector<int>& b){
+            if(a[0]==b[0])return a[1]>b[1];
+            return a[0]<b[0];
+        });
+
+        vector<int> temp;
+        for(auto &e:envelopes){
+            int h=e[1];
+            auto it=lower_bound(temp.begin(),temp.end(),h);
+            if(it==temp.end()){
+                temp.push_back(h);
+            }
+            else{
+                *it=h;
+            }
+        }
+        return temp.size();
+    }
+};
