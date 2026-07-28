3038061ea752b5b100ca1d3d5218f2a0d876851f
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-07-28 19:43:09 +0000

    Time: 28 ms (72.02%), Space: 80.7 MB (13.23%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        array<int, 26> a{};
+        vector<int> a(26);
         for (auto i : s) {
             a[i - 'a']++;
         }
         string left = "", middle;
-        left.reserve(s.size() / 2);
         for (int i = 0; i < 26; i++) {
 
-            left.append(a[i] / 2, 'a' + i);
+            left += string(a[i] / 2, 'a' + i);
 
             if (a[i] % 2 == 1) {
                 middle = 'a' + i;
