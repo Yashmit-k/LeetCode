dd85e6c49dc66ddd873fd79a6089053398aafefe
Yashmit Kotekar <153583965+Yashmit-k@users.noreply.github.com>
2026-09-21 23:51:13 +0530

    Time: 27 ms (52.62%), Space: 88.2 MB (67.75%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool check(int mid,vector<int>& c,long long k){
+        for(auto i:c){
+            k-=(i/mid);
+            if(k<=0)return true;
+        }
+        return false;
+    }
+    int maximumCandies(vector<int>& c, long long k) {
+        int l=1,r=*max_element(c.begin(),c.end());
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(check(mid,c,k))l=mid+1;
+            else r=mid-1;
+        }
+        return r;
+    }
+};
